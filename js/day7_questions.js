5e43ff584f7d1c2f2b21cea6379166c967d53bf1
Flatiron School <flatironschool@Flatirons-Air-4.home>
2015-09-04 16:29:34 -0400

    refactors the quetions and answers out into different files

---
+++ js/day7_questions.js
@@ -0,0 +1,26 @@
+// Setting up prompt questions.
+
+var whoLives;
+var whosGonnaKillYou;
+var ateBreakfast;
+var bestThings;
+var hoesClothes;
+var cantTell;
+var realShit;
+var lateness;
+var wrongs;
+
+whoLives = "Which celebrity happens to live next door to the house you just looked at the other day?";
+whosGonnaKillYou = "Who's gonna kill you for saying this, but you have to say it anyway?";
+ateBreakfast = "At which established fashion house did you have breakfast this morning?";
+bestThings = "What do they say the best things in life are?";
+hoesClothes = "When the hoes come to your party, what should they be wearing?";
+cantTell = "What can't I tell you?";
+realShit = "What are you not sure that anyone even makes anymore?";
+lateness = "Upon your late arrival to a function, what should the host be honored by?";
+wrongs = "What do you regret, but also acknowledge that that's the same thing that got you to where you are today?";
+
+//Setting up an array of all the questions.
+
+var kanyeArray = [whoLives, whosGonnaKillYou, ateBreakfast, bestThings, hoesClothes, cantTell, realShit, lateness, wrongs];
+
